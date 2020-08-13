79fb6c046eceee472df388abf86dbf1edef1953c
Juan.Hervas <juan.hervas@holascharff.com>
2020-08-13 11:46:26 -0500

    13/08 Antes de cambiar firebase

--- app/src/main/java/com/example/ruteandoapp/Controlador/Progreso.java
+++ app/src/main/java/com/example/ruteandoapp/Controlador/Progreso.java
@@ -3,6 +3,7 @@ package com.example.ruteandoapp.Controlador;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,6 +22,10 @@ public class Progreso extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    String titulo[], descrip[];
+    int images[] = {R.drawable.scharff_logo_s,R.drawable.camaras,R.drawable.extrasf};
+    RecyclerView recyclerView;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -51,10 +56,20 @@ public class Progreso extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        recyclerView = recyclerView.findViewById(R.id.retosrecycler);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        titulo = getResources().getStringArray(R.array.retos);
+        descrip = getResources().getStringArray(R.array.descripcion_retos);
+
+        RetosAdapterRecycler retosAdapterRecycler = new RetosAdapterRecycler(Progreso.this,titulo,descrip,images);
+
+
     }
 
     @Override
